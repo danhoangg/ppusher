b20937a61f628449364abacd78cfc37cfca321c9
Daniel Hoang <hoang.daniel2005@gmail.com>
2022-11-27 23:43:36 +0000

    Finished the dashboard page, needs some polishing up however everything displays fine

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const alpha = require('alphavantage')({ key: '' });
+const si = require("stock-info")
 
 var mysql = require('mysql');
 
@@ -44,28 +44,135 @@ function getTransactions(username, callback) {
     });
 }
 
+//function for seeing profit/loss of current invested
+function getProfitLoss(username, callback) {
+    con.query("SELECT OrderTable.Type, OrderTable.Ticker, OrderTable.AvgOpen, OrderTable.Invested, OrderTable.Leverage FROM UserTable JOIN OrderTable ON UserTable.UserID = OrderTable.UserID WHERE username = ?", username, function (err, result, fields) {
+        if (err) throw callback(err);
+        callback(null, result);
+    });
+}
+
+function getPrices(tickers, callback) {
+    si.getStocksInfo(tickers).then((data) => {
+        bidprices = []
+        askprices = []
+        regularchangepercents = []
+        data.forEach(element => {
+            bidprices.push(element.bid)
+            askprices.push(element.ask)
+            regularchangepercents.push(element.regularMarketChangePercent)
+        })
+        callback(null, bidprices, askprices, regularchangepercents)
+    });
+}
+
+//Current should be attuned to either bid or ask price depending on if the user is buying or selling
+function calcValue(invested, current, avgopen, leverage, type) {
+    if (type == 'buy') {
+        value = (current / avgopen) * (invested * leverage)
+    } else {
+        value = (avgopen / current) * (invested * leverage)
+    }
+    userValue = (value - (invested * (leverage - 1))) * 0.99
+    return userValue
+}
+
+//this is genuinely the weirdest way I've ever done this
+//I've had to make sure that loops through arrays happen within functions and callbacks occur in the same scope otherwise it just breaks, but it works so I'm not complaining
+//Which explains the excessive use of .forEach
+function getTotalValue(profitloss, callback) {
+    var totalinvested = 0;
+    var totalvalue = 0;
+    var stocks = [];
+    var counter = 0;
+    var currentprices = [];
+    profitloss.forEach(element => {
+        stocks.push(element.Ticker)
+    })
+    getPrices(stocks, function (err, bidprices, askprices, regularchangepercents) {
+        profitloss.forEach(element => {
+            totalinvested += element.Invested;
+            if (element.Type == "buy") {
+                totalvalue += calcValue(element.Invested, bidprices[counter], element.AvgOpen, element.Leverage, element.Type)
+                currentprices.push(bidprices[counter])
+            } else {
+                totalvalue += calcValue(element.Invested, askprices[counter], element.AvgOpen, element.Leverage, element.Type)
+                currentprices.push(askprices[counter])
+            }
+            counter++;
+        })
+        callback(null, totalinvested, totalvalue, currentprices.slice(-3), regularchangepercents)
+    });
+}
+
+function calcPercentage(value, invested) {
+    var percentage = value / invested;
+    percentage = percentage - 1
+    percentage = percentage * 100
+    return percentage
+}
+
+//so many callbacks :o
+//this looks like hell but read the function names and it should make sense
+//all this is is getting the data from the database and stock market and then rendering it on the page
+//hence fat render in middle
+function updateValues(cookie, callback) {
+    getAccountInfo(cookie, function (err, balance) {
+        if (err) throw err
+        getTransactions(cookie, function (err, transactions) {
+            if (err) throw err;
+            monthpl = balance - transactions[0].Balance
+            monthpl = monthpl.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+            if (monthpl => 0) { monthcolor = "text-success" } else { monthcolor = "text-danger" }
+            getProfitLoss(cookie, function (err, profitloss) {
+                if (err) throw err;
+                if (profitloss.length == 0) {
+                    callback(null, balance, monthpl, monthcolor, balance, 0, 0, "text-success", [])
+                }
+                getTotalValue(profitloss, function (err, totalinvested, totalvalue, currentprices, regularchangepercents) {
+                    if (err) throw err;
+                    cashavailable = (balance - totalinvested);
+                    totalpl = (totalvalue - totalinvested)
+                    if (totalpl >= 0) { currentcolor = "text-success" } else { currentcolor = "text-danger" };
+                    totalpl = totalpl.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+                    latestorders = []
+                    counter = 0;
+                    profitloss.slice(-3).forEach(element => {
+                        percentage = (regularchangepercents[counter] * 100).toFixed(2);
+                        if (percentage >= 0) {
+                            latestorders.push({ ticker: element.Ticker, type: element.Type, currentprice: currentprices[counter], percentage: percentage, percentagecolor: "text-success" })
+                        } else {
+                            latestorders.push({ ticker: element.Ticker, type: element.Type, currentprice: currentprices[counter], percentage: percentage, percentagecolor: "text-danger" })
+                        }
+                    })
+                    console.log(latestorders)
+                    callback(null, balance, monthpl, monthcolor, cashavailable, totalinvested, totalpl, currentcolor, latestorders)
+                })
+            });
+        });
+    })
+}
 
 //If user is logged in, redirect to home page, else redirect to login page
 router.get('/', (req, res) => {
     let cookie = req.cookies.username
     if (cookie) {
-
-        //so many callbacks :o
-        getAccountInfo(cookie, function (err, balance) {
+        
+        updateValues(cookie, function (err, balance, monthpl, monthcolor, cashavailable, totalinvested, totalpl, currentcolor, latestorders) {
             if (err) throw err;
-            getTransactions(cookie, function (err, transactions) {
-                if (err) throw err;
-                monthpl = balance - transactions[0].Balance
-                monthpl = monthpl.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-                if (monthpl => 0) { monthcolor = "text-success" } else { monthcolor = "text-danger" }
-                res.render("home", {
-                    username: cookie,
-                    balance: balance.toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
-                    monthpl: monthpl,
-                    monthcolor: monthcolor
-                })
-            });
+            res.render("home", {
+                username: cookie,
+                balance: balance.toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
+                monthpl: monthpl,
+                monthcolor: monthcolor,
+                cashavailable: cashavailable.toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
+                totalinvested: totalinvested.toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
+                totalpl: totalpl,
+                currentcolor: currentcolor,
+                latestorders: latestorders
+            })
         })
+
     } else {
         res.redirect('/account/login')
     }
